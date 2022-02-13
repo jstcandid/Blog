e56a8f2a418e01495836b4e5db8c405b75589d2f
zanevska <yanyazanevskaya@gmail.com>
2022-02-13 16:09:16 +0300

    +app and postlist styles

---
+++ BlogPosts/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { RootRouter } from './navigation/RootRouter';
+
+function App() {
+  return (
+    <div
+      style={{
+        maxWidth: '1569px',
+        width: '100%',
+        background: '#F8FAFE',
+      }}
+    >
+      <RootRouter />
+    </div>
+  );
+}
+
+export default App;
